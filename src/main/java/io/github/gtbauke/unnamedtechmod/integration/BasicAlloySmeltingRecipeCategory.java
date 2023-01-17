ee1132e8dbac4ec6b7a7c28469249d296dbfce15
Gustavo Teodoro Bauke <baukegustavo@gmail.com>
2023-01-17 00:10:35 -0300

    animted JEI integration for Basic Alloy Smelting Recipe

--- src/main/java/io/github/gtbauke/unnamedtechmod/integration/BasicAlloySmeltingRecipeCategory.java
+++ src/main/java/io/github/gtbauke/unnamedtechmod/integration/BasicAlloySmeltingRecipeCategory.java
@@ -1,6 +1,11 @@
 package io.github.gtbauke.unnamedtechmod.integration;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.mojang.blaze3d.vertex.PoseStack;
 import io.github.gtbauke.unnamedtechmod.UnnamedTechMod;
+import io.github.gtbauke.unnamedtechmod.block.entity.base.AlloySmelterTileBase;
 import io.github.gtbauke.unnamedtechmod.init.ModBlocks;
 import io.github.gtbauke.unnamedtechmod.recipe.BasicAlloySmelterRecipe;
 import io.github.gtbauke.unnamedtechmod.utils.AlloySmelting;
@@ -8,11 +13,16 @@ import io.github.gtbauke.unnamedtechmod.utils.ModTags;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.drawable.IDrawableStatic;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -26,9 +36,38 @@ public class BasicAlloySmeltingRecipeCategory implements IRecipeCategory<BasicAl
     private final IDrawable background;
     private final IDrawable icon;
 
+    private final int regularCookTime = AlloySmelterTileBase.BURN_TIME_STANDARD;
+    private final LoadingCache<Integer, IDrawableAnimated> cachedArrows;
+
+    protected final IDrawableStatic staticFlame;
+    protected final IDrawableAnimated animatedFlame;
+
+
     public BasicAlloySmeltingRecipeCategory(IGuiHelper helper) {
         this.background = helper.createDrawable(TEXTURE, 0, 0, 176, 82);
         this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.BASIC_ALLOY_SMELTER.get()));
+
+        this.staticFlame = helper.createDrawable(TEXTURE, 176, 0, 14, 14);
+        this.animatedFlame = helper.createAnimatedDrawable(staticFlame, 300, IDrawableAnimated.StartDirection.TOP, true);
+
+        this.cachedArrows = CacheBuilder.newBuilder()
+                .maximumSize(25)
+                .build(new CacheLoader<>() {
+                    @Override
+                    public IDrawableAnimated load(Integer cookTime) {
+                        return helper.drawableBuilder(TEXTURE, 176, 14, 24, 16)
+                                .buildAnimated(cookTime, IDrawableAnimated.StartDirection.LEFT, false);
+                    }
+                });
+    }
+
+    protected IDrawableAnimated getArrow(BasicAlloySmelterRecipe recipe) {
+        int cookTime = recipe.getCookingTime();
+        if (cookTime <= 0) {
+            cookTime = AlloySmelterTileBase.BURN_TIME_STANDARD;
+        }
+
+        return this.cachedArrows.getUnchecked(cookTime);
     }
 
     @Override
@@ -49,6 +88,40 @@ public class BasicAlloySmeltingRecipeCategory implements IRecipeCategory<BasicAl
     @Override
     public IDrawable getIcon() {
         return this.icon;
+    }
+
+    @Override
+    public void draw(BasicAlloySmelterRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack stack, double mouseX, double mouseY) {
+        animatedFlame.draw(stack, 46, 36);
+
+        IDrawableAnimated arrow = getArrow(recipe);
+        arrow.draw(stack, 79, 34);
+
+        drawExperience(recipe, stack, 10);
+        drawCookTime(recipe, stack, 55);
+    }
+
+    protected void drawExperience(BasicAlloySmelterRecipe recipe, PoseStack poseStack, int y) {
+        float experience = recipe.getExperience();
+        if (experience > 0) {
+            Component experienceString = Component.translatable("gui.jei.category.smelting.experience", experience);
+            Minecraft minecraft = Minecraft.getInstance();
+            Font fontRenderer = minecraft.font;
+            int stringWidth = fontRenderer.width(experienceString);
+            fontRenderer.draw(poseStack, experienceString, getWidth() - stringWidth - 5, y, 0xFF808080);
+        }
+    }
+
+    protected void drawCookTime(BasicAlloySmelterRecipe recipe, PoseStack poseStack, int y) {
+        int cookTime = recipe.getCookingTime();
+        if (cookTime > 0) {
+            int cookTimeSeconds = cookTime / 20;
+            Component timeString = Component.translatable("gui.jei.category.smelting.time.seconds", cookTimeSeconds);
+            Minecraft minecraft = Minecraft.getInstance();
+            Font fontRenderer = minecraft.font;
+            int stringWidth = fontRenderer.width(timeString);
+            fontRenderer.draw(poseStack, timeString, getWidth() - stringWidth - 5, y, 0xFF808080);
+        }
     }
 
     @Override
