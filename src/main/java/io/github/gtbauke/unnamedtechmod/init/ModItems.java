a18b85175f82a109f2e323f460430be2050975f3
Heymity <gabriel.prscavone@gmail.com>
2023-01-18 22:58:33 -0300

    Refactoring in ModItems and pt-br translation key

--- src/main/java/io/github/gtbauke/unnamedtechmod/init/ModItems.java
+++ src/main/java/io/github/gtbauke/unnamedtechmod/init/ModItems.java
@@ -26,78 +26,6 @@ public class ModItems {
             )
     );
 
-    public static final RegistryObject<Item> TIN_DUST = ITEMS.register(
-            "tin_dust",
-            () -> new Item(
-                    new Item.Properties()
-                            .tab(UnnamedTechMod.MOD_TAB)
-            )
-    );
-
-    public static final RegistryObject<Item> IRON_DUST = ITEMS.register(
-            "iron_dust",
-            () -> new Item(
-                    new Item.Properties()
-                            .tab(UnnamedTechMod.MOD_TAB)
-            )
-    );
-
-    public static final RegistryObject<Item> GOLD_DUST = ITEMS.register(
-            "gold_dust",
-            () -> new Item(
-                    new Item.Properties()
-                            .tab(UnnamedTechMod.MOD_TAB)
-            )
-    );
-
-    public static final RegistryObject<Item> COPPER_DUST = ITEMS.register(
-            "copper_dust",
-            () -> new Item(
-                    new Item.Properties()
-                            .tab(UnnamedTechMod.MOD_TAB)
-            )
-    );
-
-    public static final RegistryObject<Item> TIN_PLATE = ITEMS.register(
-            "tin_plate",
-            () -> new Item(
-                    new Item.Properties()
-                            .tab(UnnamedTechMod.MOD_TAB)
-            )
-    );
-
-    public static final RegistryObject<Item> IRON_PLATE = ITEMS.register(
-            "iron_plate",
-            () -> new Item(
-                    new Item.Properties()
-                            .tab(UnnamedTechMod.MOD_TAB)
-            )
-    );
-
-    public static final RegistryObject<Item> GOLD_PLATE = ITEMS.register(
-            "gold_plate",
-            () -> new Item(
-                    new Item.Properties()
-                            .tab(UnnamedTechMod.MOD_TAB)
-            )
-    );
-
-    public static final RegistryObject<Item> COPPER_PLATE = ITEMS.register(
-            "copper_plate",
-            () -> new Item(
-                    new Item.Properties()
-                            .tab(UnnamedTechMod.MOD_TAB)
-            )
-    );
-
-    public static final RegistryObject<Item> TIN_INGOT = ITEMS.register(
-            "tin_ingot",
-            () -> new Item(
-                    new Item.Properties()
-                            .tab(UnnamedTechMod.MOD_TAB)
-            )
-    );
-
     public static final RegistryObject<Item> LIGHT_CLAY_BALL = ITEMS.register(
             "light_clay_ball",
             () -> new Item(
@@ -122,9 +50,18 @@ public class ModItems {
             )
     );
 
+    public static final RegistryObject<Item> TIN_INGOT = registerIngot("tin");
     public static final RegistryObject<Item> BRONZE_INGOT = registerIngot("bronze");
     public static final RegistryObject<Item> BRONZE_PLATE = registerPlate("bronze");
+    public static final RegistryObject<Item> COPPER_PLATE = registerPlate("copper");
+    public static final RegistryObject<Item> IRON_PLATE = registerPlate("iron");
+    public static final RegistryObject<Item> GOLD_PLATE = registerPlate("gold");
+    public static final RegistryObject<Item> TIN_PLATE = registerPlate("tin");
     public static final RegistryObject<Item> BRONZE_DUST = registerDust("bronze");
+    public static final RegistryObject<Item> TIN_DUST = registerDust("tin");
+    public static final RegistryObject<Item> IRON_DUST = registerDust("iron");
+    public static final RegistryObject<Item> GOLD_DUST = registerDust("gold");
+    public static final RegistryObject<Item> COPPER_DUST = registerDust("copper");
 
     public static void register(IEventBus bus) {
         ITEMS.register(bus);
